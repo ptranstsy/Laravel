f9d86b8601aa08eb970ab3efcb33b4d5eba18076
unknown <ptranstsy08@gmail.com>
2023-05-01 20:55:24 +0700

    first commit

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+  <title>Kwangya Health</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+
+  <header>
+    <div class="topbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 text-sm">
+            <div class="site-info">
+              <a href="#"><span class="mai-call text-primary"></span> +82 123456789</a>
+              <span class="divider">|</span>
+              <a href="#"><span class="mai-mail text-primary"></span> kwangya.health@gmail.com</a>
+            </div>
+          </div>
+          <div class="col-sm-4 text-right text-sm">
+            <div class="social-mini-button">
+              <a href="#"><span class="mai-logo-facebook-f"></span></a>
+              <a href="#"><span class="mai-logo-twitter"></span></a>
+              <a href="#"><span class="mai-logo-dribbble"></span></a>
+              <a href="#"><span class="mai-logo-instagram"></span></a>
+            </div>
+          </div>
+        </div> <!-- .row -->
+      </div> <!-- .container -->
+    </div> <!-- .topbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="{{url('/')}}"><span class="text-primary">Kwangya</span>-Health</a>
+
+        
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupport">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{url('/')}}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="doctors.html">Doctors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.html">News</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+            @if(Route::has('login'))
+
+            @auth
+
+            <li class="nav-item">
+              <a class="nav-link" style="background-color: red;" href="{{url('myappointment')}}">My Appointment</a>
+            </li>
+
+            <x-app-layout>
+            </x-app-layout>
+
+            @else
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-3" href="{{route('login')}}">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-3" href="{{route('register')}}">Register</a>
+            </li>
+            @endauth
+            @endif
+          </ul>
+        </div> <!-- .navbar-collapse -->
+      </div> <!-- .container -->
+    </nav>
+  </header>
+
+  @if(session()->has('message'))
+
+<div class="alert alert-success">
+
+    <button type="button"class="close" data-dismiss="alert">
+    x
+    </button>
+    {{session()->get('message')}}
+</div>
+@endif
+
+  <div class="page-hero bg-image overlay-dark" style="background-image: url(https://i.pinimg.com/originals/f2/c4/ef/f2c4ef80cf459a1a5c6e8b1926069ccd.jpg);">
+    <div class="hero-section">
+      <div class="container text-center wow zoomIn">
+        <span class="subhead">Healthy Today, Happy Tomorrow</span>
+        <h1 class="display-4">Kwangya Health</h1>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="bg-light">
+    
+    <div class="page-section py-3 mt-md-n5 custom-index">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-secondary text-white">
+                <span class="mai-chatbubbles-outline"></span>
+              </div>
+              <p><span>Chat</span> with a doctors</p>
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-primary text-white">
+                <span class="mai-shield-checkmark"></span>
+              </div>
+              <p><span>One</span>-Health Protection</p>
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card-service wow fadeInUp">
+              <div class="circle-shape bg-accent text-white">
+                <span class="mai-basket"></span>
+              </div>
+              <p><span>One</span>-Health Pharmacy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .page-section -->
+
+    <div class="page-section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <h1>Welcome to Kwangya Health Center</h1> <br>
+            <p class="text-grey mb-4">Our sites holds a wealth of information about the services and programs we offer for our regional community  as well as general health news, a provider listing and career opportunities.</p>
+           <p class="text-grey mb-4"> I believe we have an outstanding staff – physicians, clinical and support personnel – whose ultimate goal is to provide you with the best care possible in a respectful and compassionate manner. We are continually looking for new ways to be better at what we do. Please don't hesitate to let us know what we can do to make your visit to Memorial Hospital and Health Care Center more comfortable. As a fundamental component of our mission statement, we allow ourselves to be guided by the needs of those we serve. As needs change, we will change.</p>
+<p class="text-grey mb-4">Again, thank you for allowing us to serve you during what is most likely a very vulnerable time in your life. I appreciate the significant trust you place in us!</p>
+            </p>
+
+          </div>
+          <div class="col-lg-6 wow fadeInRight" data-wow-delay="400ms">
+            <div class="img-place custom-img-1">
+              <img src="https://i.pinimg.com/originals/3b/d5/31/3bd531cbe56e4a5ab00929523f331dcf.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .bg-light -->
+  </div> <!-- .bg-light -->
+
+ @include('user.doctor')
+
+ @include('user.latest')
+
+  @include('user.appointment')
+
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row px-md-3">
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Company</h5>
+          <ul class="footer-menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Editorial Team</a></li>
+            <li><a href="#">Protection</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>More</h5>
+          <ul class="footer-menu">
+            <li><a href="#">Terms & Condition</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Advertise</a></li>
+            <li><a href="#">Join as Doctors</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Our partner</h5>
+          <ul class="footer-menu">
+            <li><a href="#">Kakao-Pharmacy</a></li>
+            <li><a href="#">JYP-Health</a></li>
+            <li><a href="#">YG-Protection</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Contact</h5>
+          <p class="footer-link mt-2">127 District 1, No 00600</p>
+          <a href="#" class="footer-link">+82 123456789</a>
+          <a href="#" class="footer-link">kwangya.health@gmail.com</a>
+
+          <h5 class="mt-3">Social Media</h5>
+          <div class="footer-sosmed mt-3">
+            <a href="#" target="_blank"><span class="mai-logo-facebook-f"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-twitter"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-google-plus-g"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-instagram"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-linkedin"></span></a>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <p id="copyright">Copyright &copy; 2020 <a href="https://macodeid.com/" target="_blank">MACode ID</a>. All right reserved</p>
+    </div>
+  </footer>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+  
+</body>
+</html>
---
+++ resources/views/user/latest.blade.php
@@ -0,0 +1,81 @@
+<div class="page-section bg-light">
+    <div class="container">
+      <h1 class="text-center wow fadeInUp">Latest News</h1>
+      <div class="row mt-5">
+        <div class="col-lg-4 py-2 wow zoomIn">
+          <div class="card-blog">
+            <div class="header">
+              <div class="post-category">
+                <a href="#">News</a>
+              </div>
+              <a href="blog-details.html" class="post-thumb">
+                <img src="https://i.pinimg.com/originals/72/c0/bb/72c0bb98bdddd6158be4bba3fded1337.jpg" alt="">
+              </a>
+            </div>
+            <div class="body">
+              <h5 class="post-title"><a href="blog-details.html">Medikids: Children's dental clinic with educational concept</a></h5>
+              <div class="site-info">
+                <div class="avatar mr-2">
+                  <div class="avatar-img">
+                    <img src="https://i.pinimg.com/originals/10/92/d8/1092d83985a972e7cb0310d97c3cbfa5.jpg" alt="">
+                  </div>
+                  <span>Lee Taeyong</span>
+                </div>
+                <span class="mai-time"></span> 2 week ago
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 py-2 wow zoomIn">
+          <div class="card-blog">
+            <div class="header">
+              <div class="post-category">
+                <a href="#">Covid19</a>
+              </div>
+              <a href="blog-details.html" class="post-thumb">
+                <img src="https://i.pinimg.com/originals/fb/54/44/fb5444894609d7649f952f114f404018.jpg" alt="">
+              </a>
+            </div>
+            <div class="body">
+              <h5 class="post-title"><a href="blog-details.html">Recovery Room: News beyond the pandemic</a></h5>
+              <div class="site-info">
+                <div class="avatar mr-2">
+                  <div class="avatar-img">
+                    <img src="https://i.pinimg.com/originals/78/c4/9b/78c49b9686523030162fade57b3a3aec.jpg" alt="">
+                  </div>
+                  <span>Giselle</span>
+                </div>
+                <span class="mai-time"></span> 4 weeks ago
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 py-2 wow zoomIn">
+          <div class="card-blog">
+            <div class="header">
+              <div class="post-category">
+                <a href="#">News</a>
+              </div>
+              <a href="blog-details.html" class="post-thumb">
+                <img src="https://i.pinimg.com/originals/ef/35/a8/ef35a87171c7af3d2ce78b0319f7c7d8.jpg" alt="">
+              </a>
+            </div>
+            <div class="body">
+              <h5 class="post-title"><a href="blog-details.html">Medikids Field Trip-where to play about health in seoul</a></h5>
+              <div class="site-info">
+                <div class="avatar mr-2">
+                  <div class="avatar-img">
+                    <img src="https://i.pinimg.com/originals/5b/f5/17/5bf5177deac038a77cec6f50f122321f.jpg" alt="">
+                  </div>
+                  <span>Ningning</span>
+                </div>
+                <span class="mai-time"></span> 2 months ago
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </div> <!-- .page-section -->
